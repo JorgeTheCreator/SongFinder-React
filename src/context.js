32518b13532d2972facbeeaf1f1281234eb5e60a
JorgeTheCreator <JrgPeguero@yahoo.com>
2018-09-03 17:15:25 -0400

    add context.js

--- src/context.js
+++ src/context.js
@@ -4,15 +4,16 @@ const Context = React.createContext();
 
 export class Provider extends Component {
   state = {
-    track_list: [
-      { track: { track_name: 'abc' } },
-      { track: { track_name: '123' } },
-      { track: { track_name: '456' } }
-    ],
+    track_list: [],
     heading: 'Top 10 Tracks'
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    axios
+      .get()
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  }
 
   render() {
     return (
